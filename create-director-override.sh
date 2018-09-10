00baddccad17b185a0b379c1d76b05e4ff39589b
Mick Pollard <aussielunix@gmail.com>
2018-09-11 03:17:18 +1000

    add S3 blobstore to the director

--- create-director-override.sh
+++ create-director-override.sh
@@ -6,11 +6,14 @@ bosh create-env \
   --vars-store  ${BBL_STATE_DIR}/vars/director-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/director-vars-file.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/aws/cpi.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/aws/s3-blobstore.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/aws/encrypted-disk.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/aws/iam-instance-profile.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/credhub.yml \
-  -o  ${BBL_STATE_DIR}/bosh-deployment/aws/iam-instance-profile.yml \
   -o  ${BBL_STATE_DIR}/bbl-ops-files/aws/bosh-director-ephemeral-ip-ops.yml \
-  -o  ${BBL_STATE_DIR}/bbl-ops-files/aws/bosh-director-encrypt-disk-ops.yml \
   -v  access_key_id="${BBL_AWS_ACCESS_KEY_ID}" \
-  -v  secret_access_key="${BBL_AWS_SECRET_ACCESS_KEY}"
+  -v  secret_access_key="${BBL_AWS_SECRET_ACCESS_KEY}" \
+  -v  s3-bucket-name="aussielunix-director-blobstore" \
+  -v  s3-region="ap-southeast-2"
